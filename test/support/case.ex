60dfc75126bf83383a0999b028ad475b99b5073e
Milton Mazzarri <milmazz@gmail.com>
2016-07-18 21:45:22 -0500

    Add Case template for tests

---
+++ test/support/case.ex
@@ -0,0 +1,46 @@
+defmodule BUPETest.Case do
+  use ExUnit.CaseTemplate
+
+  using do
+    quote do
+      import unquote(__MODULE__)
+    end
+  end
+
+  setup do
+    File.rm_rf(tmp_dir())
+    File.mkdir_p!(tmp_dir())
+  end
+
+  def tmp_dir do
+    Path.expand("../tmp/", __DIR__)
+  end
+
+  def fixtures_dir do
+    Path.expand("../fixtures", __DIR__)
+  end
+
+  def config do
+    %{
+      title: "Sample",
+      lang: "en",
+      creator: "John Doe",
+      published: "Sample",
+      date: "2016-06-23",
+      unique_identifier: "http://example.com/book/jdoe/1",
+      scheme: "URL",
+      uid: "http://example.com/book/jdoe/1",
+      files: Path.wildcard(fixtures_dir() <> "/*.html"),
+      nav: [
+        %{id: "ode-to-bacon", label: "1. Ode to Bacon", content: "bacon.html"},
+        %{id: "ode-to-ham", label: "2. Ode to Ham", content: "ham.html"},
+        %{id: "ode-to-egg", label: "3. Ode to Egg", content: "egg.html"}
+      ],
+      tmp_dir: tmp_dir()
+    }
+  end
+
+  def config(config) do
+    Map.merge(config(), config)
+  end
+end
