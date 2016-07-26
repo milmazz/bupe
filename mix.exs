4eedadfcfb696f797683db3c4dad4c07db20c8d9
Milton Mazzarri <milmazz@gmail.com>
2016-07-25 19:43:49 -0500

    Improve mix.exs conf for ExDoc

--- mix.exs
+++ mix.exs
@@ -4,11 +4,15 @@ defmodule BUPE.Mixfile do
   def project do
     [app: :bupe,
      version: "0.1.0",
+     name: "BUPE",
+     source_url: "https://github.com/milmazz/bupe",
+     homepage_url: "https://github.com/milmazz/bupe",
      elixir: "~> 1.3",
      description: description(),
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      elixirc_paths: elixirc_paths(Mix.env),
+     docs: docs(),
      package: package(),
      deps: deps()]
   end
@@ -20,23 +24,21 @@ defmodule BUPE.Mixfile do
     [applications: [:logger]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [
+      {:ex_doc, "~> 0.13", only: :dev},
+      {:earmark, "~> 1.0", only: :dev}
+    ]
   end
 
   defp description do
     """
     EPUB Generator and Parser
     """
+  end
+
+  defp docs do
+    [extras: ["README.md"], main: "README"]
   end
 
   defp package do
