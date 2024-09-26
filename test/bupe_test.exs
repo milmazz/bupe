9e3d605eb2926307f849e05a52bf661d9fad76c0
Milton Mazzarri <me@milmazz.uno>
2024-09-26 15:01:00 -0500

    validate modification date

--- test/bupe_test.exs
+++ test/bupe_test.exs
@@ -2,53 +2,57 @@ defmodule BUPETest do
   use BUPETest.Case, async: true
   doctest BUPE
 
-  test "parser should detect that file does not exists" do
-    file_path = fixtures_dir("404.epub")
-    msg = "file #{file_path} does not exists"
+  describe "pase/1" do
+    test "parser should detect that file does not exists" do
+      file_path = fixtures_dir("404.epub")
+      msg = "file #{file_path} does not exists"
 
-    assert_raise ArgumentError, msg, fn ->
-      BUPE.parse(file_path)
+      assert_raise ArgumentError, msg, fn ->
+        BUPE.parse(file_path)
+      end
+    end
+
+    test "parser should detect invalid extensions" do
+      file_path = fixtures_dir("30/bacon.xhtml")
+      msg = "file #{file_path} does not have an '.epub' extension"
+
+      assert_raise ArgumentError, msg, fn ->
+        BUPE.parse(file_path)
+      end
+    end
+
+    test "parser should report invalid EPUB mimetype" do
+      msg = "invalid mimetype, must be 'application/epub+zip'"
+
+      assert_raise RuntimeError, msg, fn ->
+        fixtures_dir("invalid_mimetype.epub") |> BUPE.parse()
+      end
     end
   end
 
-  test "parser should detect invalid extensions" do
-    file_path = fixtures_dir("30/bacon.xhtml")
-    msg = "file #{file_path} does not have an '.epub' extension"
+  describe "build/3" do
+    @tag :tmp_dir
+    test "build epub document version 3", %{tmp_dir: tmp_dir} do
+      config = config()
 
-    assert_raise ArgumentError, msg, fn ->
-      BUPE.parse(file_path)
+      output = Path.join(tmp_dir, "sample.epub")
+      {:ok, {_name, epub}} = BUPE.build(config, output, [:memory])
+
+      epub_info = BUPE.parse(epub)
+
+      assert epub_info.title == config.title
+      assert epub_info.creator == config.creator
+      assert epub_info.version == config.version
     end
-  end
 
-  test "parser should report invalid EPUB mimetype" do
-    msg = "invalid mimetype, must be 'application/epub+zip'"
+    test "builder should report invalid EPUB version" do
+      config = config()
+      config = Map.put(config, :version, "4.0")
+      msg = "invalid EPUB version, expected '2.0' or '3.0'"
 
-    assert_raise RuntimeError, msg, fn ->
-      fixtures_dir("invalid_mimetype.epub") |> BUPE.parse()
-    end
-  end
-
-  @tag :tmp_dir
-  test "build epub document version 3", %{tmp_dir: tmp_dir} do
-    config = config()
-
-    output = Path.join(tmp_dir, "sample.epub")
-    {:ok, {_name, epub}} = BUPE.build(config, output, [:memory])
-
-    epub_info = BUPE.parse(epub)
-
-    assert epub_info.title == config.title
-    assert epub_info.creator == config.creator
-    assert epub_info.version == config.version
-  end
-
-  test "builder should report invalid EPUB version" do
-    config = config()
-    config = Map.put(config, :version, "4.0")
-    msg = "invalid EPUB version, expected '2.0' or '3.0'"
-
-    assert_raise BUPE.InvalidVersion, msg, fn ->
-      BUPE.build(config, "sample.epub")
+      assert_raise BUPE.InvalidVersion, msg, fn ->
+        BUPE.build(config, "sample.epub")
+      end
     end
   end
 end
